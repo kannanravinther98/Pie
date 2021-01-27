60aec0442e2fbb90a3c783fdd4bb96e944ec5a91
Kannanak96170 <kannanravinther@gmail.com>
2021-01-28 00:10:47 +0800

    Video on homepage

---
+++ Video/app/src/main/java/com/example/video/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.video;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MediaController mediaController = new MediaController(this);
+
+        videoView = findViewById(R.id.vid);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
+        Uri uri = Uri.parse("https://www.youtube.com/watch?v=F2JJppLJIps&ab_channel=DsVirdi");
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+    }
+}
